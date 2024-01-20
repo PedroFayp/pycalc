9fb081f0a2841fa95c2fcfa800040c6d7bbee6db
Pedro Henrique <pedro_p.o@live.com>
2024-01-20 12:21:12 -0300

    test

---
+++ desafio.py
@@ -0,0 +1,12 @@
+#import TK
+from tkinter import *
+from tkinter import ttk
+
+#cor
+cor1 = "#836FFF" #azul claro
+cor2 = "#DAA520" #laranja gold
+
+window = Tk()
+window.title("Desafio - Saque")
+window.geometry("500x500")
+window.config(bg = cor1)
